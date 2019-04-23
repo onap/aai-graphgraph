4aac895516cc0d466d10e7f9235726d43466b124
Pavel Paroulek <pavel.paroulek@orange.com>
2019-04-23 15:27:28 +0200

    Adding ingestor library
    Change-Id: I830c4e5566806f14ff609e8784cd1ed2f54ba4ac
    Issue-ID: AAI-532
    Signed-off-by: Pavel Paroulek <pavel.paroulek@orange.com>

---
+++ src/main/java/org/onap/aai/graphgraph/reader/MetadataEdge.java
@@ -0,0 +1,30 @@
+package org.onap.aai.graphgraph.reader;
+
+import org.jgrapht.graph.DefaultEdge;
+
+public class MetadataEdge extends DefaultEdge {
+
+  private final String type;
+  private final String target;
+  private final String source;
+
+  public MetadataEdge(String type, String source, String target) {
+    this.source = source;
+    this.target = target;
+    this.type = type;
+  }
+
+  @Override
+  public String getTarget() {
+    return target;
+  }
+
+  @Override
+  public String getSource() {
+    return source;
+  }
+
+  public String getType() {
+    return type;
+  }
+}
