d0677635ccf0a3ea9e4530cfbd3cbd5e9e5167d3
Pavel Paroulek <pavel.paroulek@orange.com>
2019-08-26 21:45:49 +0200

    Changing port due to OOM mapping
    Change-Id: Ieb189097dacd8dd340920fa3b811e40034934028
    Issue-ID: AAI-531
    Signed-off-by: Pavel Paroulek <pavel.paroulek@orange.com>

--- graphgraph-fe/src/requests.js
+++ graphgraph-fe/src/requests.js
@@ -1,24 +1,26 @@
+const host = window.location.hostname;
+const port = 8453;
 
 export function schemas () {
-  return 'http://localhost:8080/schemas'
+  return `http://${host}:${port}/schemas`
 }
 
 export function nodeNames (schema, edgeFilter) {
-  return `http://localhost:8080/schemas/${schema}/nodes?edgeFilter=${edgeFilter}`
+  return `http://${host}:${port}/schemas/${schema}/nodes?edgeFilter=${edgeFilter}`
 }
 
 export function basicGraph (schema, node, parentHops, cousinHops, childHops, edgeFilter) {
-  return `http://localhost:8080/schemas/${schema}/graph/basic?node=${node}&parentHops=${parentHops}&cousinHops=${cousinHops}&childHops=${childHops}&edgeFilter=${edgeFilter}`
+  return `http://${host}:${port}/schemas/${schema}/graph/basic?node=${node}&parentHops=${parentHops}&cousinHops=${cousinHops}&childHops=${childHops}&edgeFilter=${edgeFilter}`
 }
 
 export function pathGraph (schema, fromNode, toNode, edgeFilter) {
-  return `http://localhost:8080/schemas/${schema}/graph/paths?fromNode=${fromNode}&toNode=${toNode}&edgeFilter=${edgeFilter}`
+  return `http://${host}:${port}/schemas/${schema}/graph/paths?fromNode=${fromNode}&toNode=${toNode}&edgeFilter=${edgeFilter}`
 }
 
 export function nodeProperty (schema, node) {
-  return `http://localhost:8080/schemas/${schema}/nodes/${node}`
+  return `http://${host}:${port}/schemas/${schema}/nodes/${node}`
 }
 
 export function edgeProperty (schema, fromNode, toNode) {
-  return `http://localhost:8080/schemas/${schema}/edges?fromNode=${fromNode}&toNode=${toNode}`
+  return `http://${host}:${port}/schemas/${schema}/edges?fromNode=${fromNode}&toNode=${toNode}`
 }
